225bd2441535ae9f0dba06d5b031aea06b3c1f37
Daniel Kioko <danielkioko1844@gmail.com>
2025-01-21 11:58:02 +0300

    Update pass_generator.py

--- pass_generator.py
+++ pass_generator.py
@@ -34,19 +34,22 @@ def copy_pwd():
         pyperclip.copy(pwd)
         lbl_alert.config(text='Succesfuly copied')
 
-text_result = tk.Label(root, text='')
+text_result = tk.Label(root, text='Length',font=("Arial", 16)
 text_result.pack()
 
 pass_length = tk.Entry(root)
 pass_length.pack()
 
-btn = tk.Button(root, text='Start', command=create_pass)
+btn = tk.Button(root, text='Start', command=create_pass,font=("Arial", 16)
 btn.pack()
 
-btn_copy = tk.Button(root, text='Copy to clipboard', command=copy_pwd)
+btn_copy = tk.Button(root, text='Copy to clipboard', command=copy_pwd,font=("Arial", 16 )
 btn_copy.pack()
 
-lbl_alert = tk.Label(root, text='')
+text_result = tk.Label(root, text='',font=("Arial", 16)
+text_result.pack()
+
+lbl_alert = tk.Label(root, text='',font=("Arial", 16)
 lbl_alert.pack()
 
 root.mainloop()
